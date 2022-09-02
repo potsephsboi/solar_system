9c15058828aa9ee50f3910fbfc26ae5e26bb8b05
potsephsboi <adpetrak@gmail.com>
2022-09-02 14:09:21 +0300

    add restart animation

--- animation.py
+++ animation.py
@@ -1,9 +1,12 @@
+from signal import pause
 from helper import *
 
 
 WIDTH = 600
 HEIGHT = 600
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+FPS = 45
 
 pygame.init()
 
@@ -13,20 +16,48 @@ def draw_win():
     WIN.fill(WHITE)
     for p in Planet.Planets:
         WIN.blit(p.icon, ((p.pos[0]-(p.size[0]/2)), p.pos[1]-(p.size[1]/2)))
+
     pygame.display.update()
 
+def move_planets(PL=Planet.Planets):
+    for p in PL:
+        if p.name != 'sun':
+            p.pos[0] += p.vx
+            p.pos[1] += p.vy
+
+def update_vel(dt, PL=Planet.Planets):
+    for p in PL:
+        if p.name != 'sun':
+            p.vx = p.vmax * math.cos(p.ang_vel * dt)
+            p.vy = p.vmax * math.sin(p.ang_vel * dt)
 
 def main():
+    s_time = time.time()
+    clock = pygame.time.Clock()
     run = True
-
+    pause = False
+    relocate_planets = False
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            if event.type == pygame.MOUSEBUTTONUP:
+                pause = not pause
+                relocate_planets = not relocate_planets
 
 
-        draw_win()
+        if not pause:
+            draw_win()
+            update_vel(time.time()-s_time)
+            move_planets()        
 
-
+        if relocate_planets:        
+            s_time = time.time()
+            Planet.Planets.clear()
+            init_planets()
+            relocate_planets = not relocate_planets
+             
 if __name__ == '__main__':
+    init_planets()
     main()
--- helper.py
+++ helper.py
@@ -1,5 +1,6 @@
 import math
 import pygame
+import time
 
 from PIL import Image
 
@@ -10,24 +11,28 @@ HEIGHT = 600
 
 class Planet:
     Planets = []
-    def __init__(self, name, icon, vx, vy, pos, size) -> None:
+    def __init__(self, name, icon, ang_vel, vx, vy, pos, size) -> None:
         self.name = name
         self.icon = icon 
+        self.ang_vel = ang_vel
         self.vx = vx
         self.vy = vy
+        self.vmax = vx
         self.pos = pos
         self.size = size
         Planet.Planets.append(self)
+
 
     def __repr__(self) -> str:
         return f'[PLANET] {self.name} || [POSITION] {self.pos} || [VELOCITY] {math.sqrt(self.vx**2 + self.vy**2)}'
 
 
-Planet('sun', pygame.image.load('../planets_animation/planets/sun.png'), 0, 0, (WIDTH/2, HEIGHT/2),
-    (int(Image.open('../planets_animation/planets/sun.png').width), int(Image.open('../planets_animation/planets/sun.png').height))
-    )
+def init_planets():
+    Planet('sun', pygame.image.load('../planets_animation/planets/sun.png'), 0, 0, 0, [WIDTH/2, HEIGHT/2],
+        (int(Image.open('../planets_animation/planets/sun.png').width), int(Image.open('../planets_animation/planets/sun.png').height))
+        )
 
-Planet('mercury', pygame.image.load('../planets_animation/planets/mercury.png'), 5, 0, (WIDTH/2, 150),
-    (int(Image.open('../planets_animation/planets/mercury.png').width), int(Image.open('../planets_animation/planets/mercury.png').height))
-    )
-# Planet('sun', pygame.image.load('/planets/sun.png'), 0, 0, (WIDTH/2, HEIGHT/2))
+    Planet('mercury', pygame.image.load('../planets_animation/planets/mercury.png'), 5, 15, 0, [WIDTH/2, 150],
+        (int(Image.open('../planets_animation/planets/mercury.png').width), int(Image.open('../planets_animation/planets/mercury.png').height))
+        )
+
